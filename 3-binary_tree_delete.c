3bea5c3203a56340aa25a81df7fe62c8062f7dcc
“bahaeeddine10” <bahaeeddine10@gmail.com>
2024-02-01 15:18:54 -0800

    0x1D. C - Binary trees

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,22 @@
+3-binary_tree_delete.c
+
+Détails
+
+Activité
+Impossible de charger les détails. Réessayez.
+
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes a binary tree.
+ * @tree: A pointer to the root node of the tree to delete.
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
+}
